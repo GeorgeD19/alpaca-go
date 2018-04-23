dea5c78d27fa0601e0da09a59fcfb8b902a291f4
George Dunlop <george.d19@hotmail.co.uk>
2018-04-23 08:14:43 +0100

    revamped parser

--- alpaca.go
+++ alpaca.go
@@ -1,136 +1,289 @@
 package alpaca
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/spf13/cast"
+	"net/http"
 
 	"github.com/buger/jsonparser"
 )
 
-// We want to build a list of fields
+type AlpacaOptions struct {
+	Schema  string
+	Data    string
+	Request *http.Request
+}
 
-// Then we want to validate each fields dependencies
+var DefaultSchemaFieldMapping = map[string]string{
+	"object": "object",
+}
 
-func FieldContainer() {
+var DefaultFormatFieldMapping = map[string]string{}
+
+type Alpaca struct {
+	data          []byte
+	schema        []byte
+	options       []byte
+	connector     string
+	request       *http.Request
+	FieldRegistry []*Field
+}
+
+type Field struct {
+	Initializing         bool
+	Parent               Alpaca
+	Data                 []byte
+	Options              []byte
+	Schema               []byte
+	Connector            *Field
+	SingleLevelRendering string
+	ID                   string
+	Key                  string
+	Title                string
+	Name                 string
+	NameCalculated       string
+	Type                 string
+	Path                 string
+	Validation           string
+	Events               string
+	ShowingDefaultData   string
+	PreviouslyValidated  bool
+	IsContainerField     bool
+	PropertyId           string
+	Value                interface{}
+	ChildrenByPropertyId map[string]Alpaca
+	childrenById         map[string]Alpaca
+	Children             []Alpaca
+	Debug                bool
+	Order                int
+	ReadOnly             bool
+	notTopLevel          bool
+	// Media                []ImageFile
+}
+
+var (
+	ErrDefaultError = errors.New("You must supply at least one argument.")
+)
+
+func Quote(a string) string {
+	return "\"" + a + "\""
+}
+
+// New initalizes and returns new alpaca parser
+func New(options AlpacaOptions) (*Alpaca, error) {
+
+	if (AlpacaOptions{}) == options {
+		return nil, ErrDefaultError
+	}
+
+	alpaca := &Alpaca{}
+
+	schemaValue, schemaType, _, _ := jsonparser.Get([]byte(options.Schema), "schema")
+	if schemaType != jsonparser.NotExist {
+		alpaca.schema = schemaValue
+	}
+
+	optionsValue, optionsType, _, _ := jsonparser.Get([]byte(options.Schema), "options")
+	if optionsType != jsonparser.NotExist {
+		alpaca.options = optionsValue
+	} else {
+		alpaca.options = []byte("{}")
+	}
+
+	dataValue, dataType, _, _ := jsonparser.Get([]byte(options.Data))
+	if dataType != jsonparser.NotExist {
+		alpaca.data = dataValue
+	} else {
+		alpaca.data = []byte("{}")
+	}
+
+	alpaca.CreateFieldInstance("", alpaca.data, alpaca.options, alpaca.schema, nil)
+
+	return alpaca, nil
+}
+
+// Parse takes field registry and parses it into json
+func (a *Alpaca) Parse() {
 
 }
 
-func FieldAny() {
+// CreateFieldInstance returns a new instance of the desired field based on the schema
+func (a *Alpaca) CreateFieldInstance(key string, data []byte, options []byte, schema []byte, connector *Field) {
+	fieldType := ""
 
-}
+	_, optionsType, _, _ := jsonparser.Get(options, "type")
+	if optionsType == jsonparser.NotExist {
 
-// GetSchemaType returns schema type of a variable.
-func GetSchemaType(data string) string {
-	var schemaType = "any"
+		// if nothing passed in, we can try to make a guess based on the type of data
+		_, schemaType, _, _ := jsonparser.Get(schema, "type")
+		if schemaType == jsonparser.NotExist {
+			_, dataType, _, _ := jsonparser.Get(data)
+			if dataType != jsonparser.NotExist {
+				jsonparser.Set(schema, []byte(string(dataType)), "type")
+			}
+		}
 
-	// if isArray(Data) == true {
-	// 	SchemaType = "array"
-	// } else if isObject(Data) == true {
-	// 	SchemaType = "object"
-	// } else if isNumber(Data) == true {
-	// 	SchemaType = "number"
-	// } else if isBoolean(Data) == true {
-	// 	SchemaType = "boolean"
-	// } else {
-	// 	SchemaType = "string"
-	// }
+		// if nothing passed in, fallback to defaults
+		if schemaType == jsonparser.NotExist {
+			fieldType = "object"
+		}
 
-	return schemaType
-}
+		// using what we now about schema, try to guess the type
+		guessedOptionType := a.GuessOptionsType(schema)
+		if guessedOptionType != "" {
+			options, _ = jsonparser.Set(options, []byte(Quote(guessedOptionType)), "type")
+		}
+	}
 
-func (f *Field) Construct() []*Field {
-	fmt.Println(f.Type)
-	switch f.Type {
+	optionsValue, optionsValueType, _, _ := jsonparser.Get(options, "type")
+
+	if optionsValueType != jsonparser.NotExist {
+		fieldType = string(optionsValue)
+	}
+	// TODO Add non container fields to field registry
+
+	switch fieldType {
 	case "object":
-		f.GetProperties()
+
+		a.Object(schema, options, data, connector)
+		break
+	case "information":
+
+		break
+	default:
+		a.Any()
 		break
 	}
 }
 
-func (f *Field) GetProperties() []*Field {
-	properties, dataType, _, _ := jsonparser.Get([]byte(f.Schema), "properties")
-	if dataType != jsonparser.NotExist {
-		jsonparser.ObjectEach([]byte(properties), func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
-			fmt.Printf("Key: '%s'\n Value: '%s'\n Type: %s\n", string(key), string(value), dataType)
+func (a *Alpaca) ResolvePropertySchemaOptions(key string, connector *Field) {
 
+	// propertyValue := make([]byte, 0)
+	propertiesValue, propertiesType, _, _ := jsonparser.Get(connector.Schema, "properties")
+	if propertiesType != jsonparser.NotExist {
+		propertyValue, propertyType, _, _ := jsonparser.Get(propertiesValue, key)
+		fmt.Println(string(propertyValue))
+		fmt.Println(propertyType)
+
+	}
+
+	options := connector.Options
+	optionsValue, optionsType, _, _ := jsonparser.Get(options, "fields")
+	if optionsType != jsonparser.NotExist {
+		propertyOptions, propertyOptionsType, _, _ := jsonparser.Get(optionsValue, string(key))
+		if propertyOptionsType != jsonparser.NotExist {
+			options = propertyOptions
+		}
+	}
+
+	fmt.Println(string(options))
+
+	// If field is found use that otherwise dive deeper
+	propertyData := connector.Data
+	propertyDataValue, propertyDataType, _, _ := jsonparser.Get(propertyData, string(key))
+	if propertyDataType != jsonparser.NotExist {
+		propertyData = propertyDataValue
+	}
+
+	fmt.Println(string(propertyData))
+	// PropertyOptions := gabs.New()
+	// PropertyOptions = ContainerField.Options
+	// if ContainerField.Options.Exists("fields") == true && ContainerField.Options.Search("fields").Exists(PropertyId) == true {
+	// 	PropertyOptions = ContainerField.Options.Search("fields").Search(PropertyId)
+	// }
+
+	// if connector.Schema  .Schema.Exists("properties") == true && ContainerField.Schema.Search("properties").Exists(PropertyId) == true {
+	// 	PropertySchema = ContainerField.Schema.Search("properties").Search(PropertyId)
+	// }
+
+	// Push through ResolvePropertySchemaOptions instead
+	// fieldSchema := value
+
+	// fieldOptions := options
+	// optionsValue, optionsType, _, _ := jsonparser.Get(options, string(key))
+	// if optionsType != jsonparser.NotExist {
+	// 	fieldOptions = optionsValue
+	// }
+
+	// fieldData := data
+	// need to dig into fields
+
+	// dataValue, dataType, _, _ := jsonparser.Get(data, string(key))
+	// if optionsType != jsonparser.NotExist {
+	// 	fieldData = dataValue
+	// }
+
+	// fmt.Printf("Key: '%s'\n Value: '%s'\n Type: %s\n", string(key), string(value), dataType)
+	// if index == 0 {
+	// a.CreateFieldInstance(string(key), fieldData, fieldOptions, fieldSchema, connector)
+	// fmt.Println(string(key))
+	// fmt.Println(string(fieldSchema))
+	// fmt.Println(string(fieldOptions))
+	// fmt.Println(string(fieldData))
+	// fmt.Println(offset)
+	// }
+}
+
+func (a *Alpaca) Object(schema []byte, options []byte, data []byte, connector *Field) {
+
+	field := &Field{
+		Schema:  schema,
+		Options: options,
+		Data:    data,
+	}
+	a.FieldRegistry = append(a.FieldRegistry, field)
+
+	propertiesValue, propertiesType, _, _ := jsonparser.Get(schema, "properties")
+	if propertiesType != jsonparser.NotExist {
+		jsonparser.ObjectEach(propertiesValue, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
+			a.ResolvePropertySchemaOptions(string(key), field)
 			return nil
 		})
 	}
 }
 
-func CreateFieldInstance(schema string, options string, data string) []*Field {
-	field := new(Field)
-	field.Schema = schema
-	field.Options = options
-	field.Data = data
+func (a *Alpaca) Information() *Field {
+	fmt.Println("information")
+	return &Field{}
+}
 
-	fieldType, dataType, _, _ := jsonparser.Get([]byte(options), "type")
-	if dataType == jsonparser.NotExist {
-		fieldType, dataType, _, _ = jsonparser.Get([]byte(schema), "type")
-		if dataType == jsonparser.NotExist {
-			// Rely on data to determine type
-			fieldType = []byte(GetSchemaType(data))
+func (a *Alpaca) Any() *Field {
+	return &Field{}
+}
+
+// Makes a best guess at the options field type if none provided.
+func (a *Alpaca) GuessOptionsType(schema []byte) string {
+	optionsType := ""
+
+	enumValue, enumType, _, _ := jsonparser.Get(schema, "enum")
+
+	if enumType != jsonparser.NotExist {
+		if enumType == jsonparser.Array {
+			if len(enumValue) > 3 {
+				optionsType = "select"
+			} else {
+				optionsType = "radio"
+			}
+		}
+	} else {
+		schemaTypeValue, schemaType, _, _ := jsonparser.Get(schema, "type")
+
+		if schemaType != jsonparser.NotExist {
+			mapValue, isset := DefaultSchemaFieldMapping[string(schemaTypeValue)]
+			if isset {
+				optionsType = mapValue
+			}
 		}
 	}
 
-	field.Type = cast.ToString(fieldType)
-	fields = append(fields, field.Construct())
-
-	return fields
-}
-
-func CheckDependencies() {
-
-}
-
-// New returns a pointer to a new Alpaca instance. Its methods are subsequently
-// called to produce a single Alpaca object.
-func New(logic string, data string) (a *Alpaca) {
-	return AlpacaNew(logic, data)
-}
-
-func AlpacaNew(logic string, data string) (a *Alpaca) {
-	a = new(Alpaca)
-	schema := "{}"
-	options := "{}"
-
-	jsonparser.ObjectEach([]byte(logic), func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
-		if string(key) == "schema" {
-			schema = string(value)
+	// check if it has format defined
+	schemaFormatValue, schemaFormatType, _, _ := jsonparser.Get(schema, "format")
+	if schemaFormatType != jsonparser.NotExist {
+		mapValue, isset := DefaultFormatFieldMapping[string(schemaFormatValue)]
+		if isset {
+			optionsType = mapValue
 		}
+	}
 
-		if string(key) == "options" {
-			options = string(value)
-		}
-
-		return nil
-	})
-
-	a.Fields = append(a.Fields, CreateFieldInstance(schema, options, data))
-
-	return
+	return optionsType
 }
-
-// func Apply(logic string, data string) (a *Alpaca) {
-// 	a = new(Alpaca)
-// 	schema := "{}"
-// 	options := "{}"
-
-// 	jsonparser.ObjectEach([]byte(logic), func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
-// 		if string(key) == "schema" {
-// 			schema = string(value)
-// 		}
-
-// 		if string(key) == "options" {
-// 			options = string(value)
-// 		}
-
-// 		a.CreateFieldInstance(schema, options, data)
-
-// 		// fmt.Printf("Key: '%s'\n Value: '%s'\n Type: %s\n", string(key), string(value), dataType)
-// 		return nil
-// 	})
-
-// 	return
-// }
--- def.go
+++ def.go
@@ -1,39 +1,37 @@
 package alpaca
 
-import "time"
+// type Alpaca struct {
+// 	Data   string
+// 	Fields []*Field
+// 	Media  map[string]File
+// }
 
-type Alpaca struct {
-	Data   string
-	Fields []*Field
-	Media  map[string]File
-}
+// // File type is a common base for files
+// type File struct {
+// 	Data    string
+// 	Type    string
+// 	Mime    string
+// 	Field   string
+// 	Name    string
+// 	Created time.Time
+// }
 
-// File type is a common base for files
-type File struct {
-	Data    string
-	Type    string
-	Mime    string
-	Field   string
-	Name    string
-	Created time.Time
-}
+// // ImageFile type extends File type to track width & height of image.
+// type ImageFile struct {
+// 	File
+// 	FieldKey string
+// 	Width    int
+// 	Height   int
+// }
 
-// ImageFile type extends File type to track width & height of image.
-type ImageFile struct {
-	File
-	FieldKey string
-	Width    int
-	Height   int
-}
-
-type Field struct {
-	Parent  *Field
-	Schema  string
-	Options string
-	Data    string
-	Name    string
-	Key     string
-	Path    string
-	Type    string
-	Value   interface{}
-}
+// type Field struct {
+// 	Parent  *Field
+// 	Schema  string
+// 	Options string
+// 	Data    string
+// 	Name    string
+// 	Key     string
+// 	Path    string
+// 	Type    string
+// 	Value   interface{}
+// }
